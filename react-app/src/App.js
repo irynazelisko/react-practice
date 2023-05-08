60d8ea15272bda583caa9e621a0ff173e6532299
Iryna <irazelisko@gmail.com>
2023-05-08 14:48:13 +0300

    lesson-38: work with state

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,29 +1,44 @@
+import { useState } from 'react';
 import { Contacts } from './components/Contacts';
 import { Experience } from './components/Experience';
 import { MyPhoto } from './components/MyPhoto';
-import '../src/styles.scss/styles.css'
+import { Counter } from './components/Counter';
+import  WorkExperienceDetails  from './components/WorkExperienceDetails';
+import  AddExperienceForm  from './components/AddExperienceForm';
+
+import '../src/styles.scss/styles.css';
 
 const photoLink = './my-photo.jpg';
 
 function App() {
-  return (
+  const [workExperienceItems, setWorkExperienceItems] = useState([]);
+
+  function addWorkExperience(newItem) {
+    setWorkExperienceItems([...workExperienceItems, newItem]);
+  }
+
+    return (
     <div className="my-theme">
-  <div className="wrapper">
-
-    <div className="side-block">
-      <div className="fixed-block">
-          <div className='images'>
-              <MyPhoto imageLink = {photoLink}/>
+      <div className="wrapper">
+        <div className="side-block">
+          <div className="fixed-block">
+            <div className='images'>
+              <MyPhoto imageLink={photoLink}/>
+            </div>
+            <Contacts/>
+      
           </div>
-          <Contacts/>
-          </div>
-          </div>
-
-          <div className="central-block">
+        </div>
+        <div className="central-block">
           <Experience/>
+          {workExperienceItems.map(item => (
+            <WorkExperienceDetails key={item.name} {...item} />
+          ))}
+          <AddExperienceForm addExperience={addWorkExperience}/>
+          <Counter/>
+        </div>
       </div>
-      </div>
-  </div>
+    </div>
   )
 }
 export default App;
--- react-app/src/components/Experience.js
+++ react-app/src/components/Experience.js
@@ -1,12 +1,28 @@
+import React, { useState } from "react";
+import WorkExperienceDetails from "./WorkExperienceDetails";
+
 export function Experience() {
+  const [experience] = useState([
+    {
+      title: 'Trainee frontend developer',
+      name: 'Robot-dreams',
+      description: 'actively learning basics of html, css, javascript, react. Getting a lot of knowledge, additional English lessons and many other useful opportunities',
+      link: 'https://robotdreams.cc/uk?utm_term=robotdreams&utm_campaign=robot_dreams&utm_source=google&utm_medium=cpc&utm_content=search-eu&gad=1&gclid=Cj0KCQjwu-KiBhCsARIsAPztUF1j8UOAw_pFB43-1MEpCJcKEmXTSjyWcxOv4rkLTcN9wOt5NRZy-UEaAitlEALw_wcB'
+    }
+  ]);
     return (
       <div className="work">
         <h3 className="central-title central-border" id="sectionC">Work experience</h3>
         <div className="work-descr">
-          <span className="bold">Name of employer:</span> Robot-dreams <br />
-          <span className="bold">Dates of employment:</span> 28.11-present <br />
-          <span className="bold">Job title:</span> Trainee frontend developer <br />
-          <span className="bold">Description:</span> actively learning basics of html, css, javascript. Getting a lot of knowledge, additional English lessons and many other useful opportunities
+        {experience.map((item, index) => (
+          <WorkExperienceDetails
+            key={index}
+            title={item.title}
+            name={item.name}
+            description={item.description}
+            link={item.link}
+          />
+          ))};
         </div>
       </div>
     );
