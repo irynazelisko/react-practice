9d326d8266418494f4d4731c1496484ec065e71e
Iryna <irazelisko@gmail.com>
2023-05-08 14:58:18 +0300

    update code to lesson-38

--- react-app/src/components/Experience.js
+++ react-app/src/components/Experience.js
@@ -22,7 +22,7 @@ export function Experience() {
             description={item.description}
             link={item.link}
           />
-          ))};
+          ))}
         </div>
       </div>
     );
